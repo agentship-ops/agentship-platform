25c8fac7834a0da12481d7cc6555d58c0bc851ff
agentship-ops <operations@agentship.com>
2026-07-02 13:15:47 -0400

    Create TrainingDetail.jsx

---
+++ src/components/TrainingDetail.jsx
@@ -0,0 +1,9 @@
+import TrainingDetail from '../components/TrainingDetail'
+
+// in VIEWS object, replace these lines:
+mastery: <TrainingDetail slug="mastery" />,
+'tlib-1': <TrainingDetail slug="tlib-1" />,
+'tlib-2': <TrainingDetail slug="tlib-2" />,
+'tlib-3': <TrainingDetail slug="tlib-3" />,
+'tlib-4': <TrainingDetail slug="tlib-4" />,
+'tlib-5': <TrainingDetail slug="tlib-5" />,
